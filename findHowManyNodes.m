98e70e286e8b95636dd78734ae1aa26dca782571
Roberto Ariosa <rob.ariosa@hotmail.com>
2017-03-03 20:08:47 +0100

    Find how many nodes algorithm
    It basically searchs for the number of nodes needed to get to any
    location of the map

---
+++ findHowManyNodes.m
@@ -0,0 +1,13 @@
+while isempty(path)
+    % No feasible path found yet, increase the number of nodes
+    prm.NumNodes = prm.NumNodes + 10;
+
+    % Use the |update| function to re-create the PRM roadmap with the changed
+    % attribute
+    update(prm);
+
+    % Search for a feasible path with the updated PRM
+    path = findpath(prm, startLocation, endLocation);
+end
+% Display path
+path
